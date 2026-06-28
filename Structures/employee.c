7e5ec76b633be1fe4053dcc9a10b663fd07089c4
nikithakatukuri <nikithakatukuri12@gmail.com>
2026-06-28 19:30:10 +0530

    Add employee structure and input/output functionality

---
+++ Structures/employee.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct Employee{
+    int id;
+    char name[50];
+    float salary;
+};
+int main(){
+    struct Employee e;
+    printf("Enter Employee Id: ");
+    scanf("%d",&e.id);
+    printf("Enter Name: ");
+    scanf("%s",&e.name);
+    printf("Enter Salary: ");
+    scanf("%f",&e.salary);
+    printf("\nEmployee Details\n");
+    printf("ID: %d\n",e.id);
+    printf("Name: %s\n",e.name);
+    printf("Salary: %.2f\n",e.salary);
+}
